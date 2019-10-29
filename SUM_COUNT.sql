1b34aef7010c8b34461347b2fa008026062b647f
J. Christian Romero <jcromerohdz@gmail.com>
2019-10-29 14:07:22 -0700

    Adding changes to readme file

--- SUM_COUNT.sql
+++ SUM_COUNT.sql
@@ -26,3 +26,15 @@ WHERE name in ('Estonia', 'Latvia', 'Lithuania');
 SELECT continent, COUNT(name)
 FROM world
 GROUP BY continent
+
+-- For each continent show the continent and number of countries with populations of at least 10 million.
+SELECT continent, COUNT(name)
+FROM world
+WHERE population >= 10000000
+GROUP BY continent;
+
+-- List the continents that have a total population of at least 100 million.
+SELECT continent
+FROM world
+GROUP BY continent
+HAVING SUM(population) > 100000000;
